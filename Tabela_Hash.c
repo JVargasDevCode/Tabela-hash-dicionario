0eac215aeae90095a8d2e4eb88acd30b1c3f132d
Julio Vargas <115955332+JVargasDevCode@users.noreply.github.com>
2026-06-03 14:45:30 -0300

    Update Tabela_Hash.c

--- Tabela_Hash.c
+++ Tabela_Hash.c
@@ -160,3 +160,57 @@ void exibirEstatisticas() {
 	printf("Fator de carga: %.2f\n", fatorCarga);
 	printf("Maior lista encadeada: %d\n", maiorLista);
 }
+void carregarPalavrasIniciais() {
+
+	inserir("Computador",
+	        "Maquina capaz de processar dados.");
+
+	inserir("Algoritmo",
+	        "Sequencia de passos para resolver um problema.");
+
+	inserir("Hash",
+	        "Estrutura usada para armazenamento rapido.");
+
+	inserir("Ponteiro",
+	        "Variavel que armazena endereco de memoria.");
+
+	inserir("Compilador",
+	        "Programa que traduz codigo fonte.");
+}
+
+void liberarMemoria() {
+
+	for(int i = 0; i < TAM; i++) {
+
+		Palavra *aux = tabela[i].lista;
+
+		while(aux != NULL) {
+
+			Palavra *temp = aux;
+			aux = aux->prox;
+			free(temp);
+		}
+	}
+}
+
+
+case 4:
+			exibirTabela();
+			break;
+
+		case 5:
+			exibirEstatisticas();
+			break;
+
+		case 0:
+			printf("Sistema encerrado.\n");
+			break;
+
+		default:
+			printf("Opcao invalida!\n");
+		}
+
+	} while(opcao != 0);
+	liberarMemoria();
+	return 0;
+}
